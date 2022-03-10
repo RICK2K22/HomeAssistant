87bea18d88d94868e1b8b1b170e05006398f7a4c
Diego Damaceno dos Santos <78373208+diego780705@users.noreply.github.com>
2022-03-10 08:24:38 -0300

    Update InstalaçãoUbuntu.sh

--- InstalaçãoUbuntu.sh
+++ InstalaçãoUbuntu.sh
@@ -2,7 +2,11 @@
 
 sudo -i
 
+sleep 2
+
 apt update -y && sudo apt dist-upgrade -y && sudo apt autoremove -y 
+
+sleep 2
 
 apt-get install \
 jq \
@@ -17,22 +21,42 @@ vim \
 openssh-server \
 dbus -y
 
+sleep 5
+
 curl -fsSL get.docker.com | sh
+
+sleep 5
 
 wget https://github.com/diego780705/HomeAssistant/raw/a13052d267deeccb6814d47d5f6570ba9536f4e0/os-agent_1.2.2_linux_x86_64.deb
 
+sleep 5
+
 dpkg -i os-agent_1.2.2_linux_x86_64.deb
+
+sleep 5
 
 wget https://github.com/diego780705/HomeAssistant/raw/a13052d267deeccb6814d47d5f6570ba9536f4e0/homeassistant-supervised.deb
 
+sleep 5
+
 dpkg -i homeassistant-supervised.deb
+
+sleep 10
 
 systemctl enable firewalld
 
+sleep 2
+
 systemctl start firewalld 
+
+sleep 2
 
 firewall-cmd --state
 
+sleep 2
+
 firewall-cmd --permanent --add-port=1-9999/tcp
+
+sleep 2
 
 firewall-cmd --reload
